074a344183db864395f58c74625f13f24135ce24
Miika <vainiomiika@hotmail.com>
2024-11-04 19:43:05 +0200

    Added Navbar component with its own scss file. Added few photos for further process. Navbar still needs modification

---
+++ app/components/navbar.tsx
@@ -0,0 +1,27 @@
+// src/components/Navbar.tsx
+import React from 'react';
+import Image from 'next/image';
+import '../styles/navbar.scss';
+import logo from '../photos/logo.png';
+
+const Navbar = () => {
+  return (
+    <div className="navbar">
+      <div className="navbar-logo">
+        <Image src={logo} alt="LocalZero Logo" className="logo-image"/>
+        <div className="logo-text-container">
+          <span className="logo-text">LocalZero</span>
+          <span className="logo-subtext">TOP-Maßnahmen</span>
+        </div>
+      </div>
+      <div className="navbar-links">
+        <a href="#about">ÜBER LOCALZERO</a>
+        <a href="#monitoring">MONITORING</a>
+        <a href="#klimaweg">KLIMAWEG</a>
+      </div>
+      <button className="donate-button">JETZT SPENDEN</button>
+    </div>
+  );
+};
+
+export default Navbar;
--- app/page.tsx
+++ app/page.tsx
@@ -1,44 +1,40 @@
 // src/pages.tsx
 "use client";
 import React, { useState } from "react";
+import Navbar from "./components/navbar";
 import FilterPanel from "./components/FilterPanel";
 import MeasuresGrid from "./components/MeasuresGrid";
 import "./styles/styles.scss";
 import Footer from "@/app/components/Footer";
 
-// Define the type for the filter change handler
-interface FilterChangeHandler {
-  priorities: number[];
-  sectors: string[];
-}
-
 const Pages = () => {
   const [selectedPriorities, setSelectedPriorities] = useState<number[]>([]);
   const [selectedSectors, setSelectedSectors] = useState<string[]>([]);
 
-  // Handle changes from the FilterPanel for both priorities and sectors
   const handleFilterChange = (priorities: number[], sectors: string[]) => {
     setSelectedPriorities(priorities);
     setSelectedSectors(sectors);
   };
 
-    return (
-        <div className="d-flex flex-column flex-grow-1">
-            <div className="app flex-grow-1">
-                <div className="sidebar">
-                    <FilterPanel onFilterChange={handleFilterChange}/>
-                </div>
-                <div className="main-content">
-                    <h1>TOP-MASSNAHMEN</h1>
-                    <MeasuresGrid
-                        selectedPriorities={selectedPriorities}
-                        selectedSectors={selectedSectors}
-                    />
-                </div>
-            </div>
-            <Footer/>
+  return (
+    <div className="d-flex flex-column flex-grow-1">
+      <Navbar />
+      
+      <div className="app flex-grow-1">
+        <div className="sidebar">
+          <FilterPanel onFilterChange={handleFilterChange} />
         </div>
-    );
+        <div className="main-content">
+          <h1>TOP-MASSNAHMEN</h1>
+          <MeasuresGrid
+            selectedPriorities={selectedPriorities}
+            selectedSectors={selectedSectors}
+          />
+        </div>
+      </div>
+      <Footer />
+    </div>
+  );
 };
 
 export default Pages;
