6569ff68dec02f531a03767058fc39e4f47047f7
Juuso Mannström <juuso.mannstrom@tuni.fi>
2024-11-04 20:27:00 +0200

    Changes to focus filtering, added bookmark button

--- app/page.tsx
+++ app/page.tsx
@@ -1,44 +1,51 @@
 // src/pages.tsx
-"use client";
-import React, { useState } from "react";
-import FilterPanel from "./components/FilterPanel";
-import MeasuresGrid from "./components/MeasuresGrid";
-import "./styles/styles.scss";
-import Footer from "@/app/components/Footer";
+'use client';
+import React, { useState } from 'react';
+import FilterPanel from './components/FilterPanel';
+import MeasuresGrid from './components/MeasuresGrid';
+import './styles/styles.scss';
+import Footer from '@/app/components/Footer';
 
 // Define the type for the filter change handler
 interface FilterChangeHandler {
   priorities: number[];
   sectors: string[];
+  focuses: string[];
 }
 
 const Pages = () => {
   const [selectedPriorities, setSelectedPriorities] = useState<number[]>([]);
   const [selectedSectors, setSelectedSectors] = useState<string[]>([]);
+  const [selectedFocuses, setSelectedFocus] = useState<string>('');
 
   // Handle changes from the FilterPanel for both priorities and sectors
-  const handleFilterChange = (priorities: number[], sectors: string[]) => {
+  const handleFilterChange = (
+    priorities: number[],
+    sectors: string[],
+    focus: string
+  ) => {
     setSelectedPriorities(priorities);
     setSelectedSectors(sectors);
+    setSelectedFocus(focus);
   };
 
-    return (
-        <div className="d-flex flex-column flex-grow-1">
-            <div className="app flex-grow-1">
-                <div className="sidebar">
-                    <FilterPanel onFilterChange={handleFilterChange}/>
-                </div>
-                <div className="main-content">
-                    <h1>TOP-MASSNAHMEN</h1>
-                    <MeasuresGrid
-                        selectedPriorities={selectedPriorities}
-                        selectedSectors={selectedSectors}
-                    />
-                </div>
-            </div>
-            <Footer/>
+  return (
+    <div className='d-flex flex-column flex-grow-1'>
+      <div className='app flex-grow-1'>
+        <div className='sidebar'>
+          <FilterPanel onFilterChange={handleFilterChange} />
         </div>
-    );
+        <div className='main-content'>
+          <h1>TOP-MASSNAHMEN</h1>
+          <MeasuresGrid
+            selectedPriorities={selectedPriorities}
+            selectedSectors={selectedSectors}
+          />
+        </div>
+      </div>
+      <Footer />
+    </div>
+  );
 };
 
 export default Pages;
